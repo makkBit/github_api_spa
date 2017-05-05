918849a1aa6c408cdd24ef9b491dc7e591faff6a
Makk <makk.bit@outlook.com>
2017-05-05 19:32:01 +0530

    make production ready

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import './index.css';
 
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path="/" component={App}>
+    <Route path={process.env.PUBLIC_URL + '/'} component={App}>
     	<IndexRoute component={Search} />
     	<Route path="/user/:username" component={User} />
 	</Route>
